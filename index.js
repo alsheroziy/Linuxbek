683d964ed3ade9685e73048a1d3db86e8c621e0b
Shehroz Raxmatov <raxmatshehroz1@gmail.com>
2024-02-11 23:41:05 +0500

    server

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+app.use("/", (req, res, next)=>{
+  res.send("ho")
+})
 
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
